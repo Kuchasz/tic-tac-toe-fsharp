18755cdd895d7688174ac15db821e89376f489d2
Rafal Pysz <kuchasz1@o2.pl>
2018-12-26 02:03:44 +0100

    initial game state created

---
+++ Model.fs
@@ -0,0 +1,38 @@
+module Model
+
+type VerticalPosition = Top | Center | Bottom
+type HorizontalPosition = Left | Center | Right
+
+type Position = {
+    Vertical: VerticalPosition
+    Horizontal: HorizontalPosition
+}
+
+type Player = 
+    | X 
+    | O
+
+type FieldState = 
+    | Empty 
+    | Player of Player
+
+type Field = {
+    FieldState: FieldState
+    Position: Position
+}
+
+type Move = {
+    Player: Player
+    Position: Position
+}
+
+type Board = {
+    Fields: Field list
+}
+
+let play (board: Board) move =
+    let playedField = { FieldState = Player move.Player; Position = move.Position }
+    { Fields = Seq.append board.Fields [playedField] |> Seq.toList }
+
+let start = 
+    { Fields = []}
---
+++ Program.fs
@@ -0,0 +1,18 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Model
+
+[<EntryPoint>]
+let main argv =
+    let game = start
+    printfn "%A" game
+    let s1 = play game { Position = { Vertical = VerticalPosition.Top; Horizontal = HorizontalPosition.Left }; Player = Player.X }
+    printfn "%A" s1
+    let s2 = play s1 { Position = { Vertical = VerticalPosition.Center; Horizontal = HorizontalPosition.Center }; Player = Player.O }
+    printfn "%A" s2
+    let s3 = play s2 { Position = { Vertical = VerticalPosition.Bottom; Horizontal = HorizontalPosition.Left }; Player = Player.X }
+    printfn "%A" s3
+    let s4 = play s3 { Position = { Vertical = VerticalPosition.Center; Horizontal = HorizontalPosition.Left }; Player = Player.O }
+    printfn "%A" s4
+    0 // return an integer exit code
