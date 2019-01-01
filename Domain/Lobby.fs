2889f126d7ee0cf5333f6239c112474f2bdcbc0b
Rafal Pysz <kuchasz1@o2.pl>
2019-01-01 20:22:51 +0100

    firther refactorings

--- Domain/Lobby.fs
+++ Domain/Lobby.fs
@@ -1,2 +1,9 @@
 module Lobby
 
+open SimpleFp
+open Player
+
+let join planerName =
+    match createPlayerName planerName with
+    | Success name -> Success { Id = PlayerId "blah"; Name = name }
+    | Failure err -> Failure err
--- Domain/Player.fs
+++ Domain/Player.fs
@@ -1,3 +1,17 @@
 module Player 
 
+open SimpleFp
+
 type PlayerId = PlayerId of string
+
+type PlayerName = PlayerName of string
+
+type Player = {
+    Id: PlayerId
+    Name: PlayerName
+}
+
+let createPlayerName (name: string) =
+    match name.Length >=5 with
+    | true -> Success (PlayerName name)
+    | false -> Failure "Player name is too short"
