1ab2324ce8fc6e9b9914f57bc4cc2777cf1f3956
Rafal Pysz <kuchasz1@o2.pl>
2019-01-05 00:27:29 +0100

    added interop to Player Domain

--- Domain/Lobby.fs
+++ Domain/Lobby.fs
@@ -2,8 +2,16 @@ module Domain.Lobby
 
 open Tools.Functional
 open Domain.Player
+open Domain.Game
+open System
+
+let createGuid =
+    Guid.NewGuid().ToString()
 
 let join planerName =
     match createPlayerName planerName with
-    | Success name -> Success { Id = PlayerId "blah"; Name = name }
+    | Success name -> Success { Id = PlayerId createGuid; Name = name }
     | Failure err -> Failure err
+
+let createGamePlayer designation player =
+    { Designation = designation; PlayerId = player.Id }
--- Program.fs
+++ Program.fs
@@ -1,22 +1,40 @@
 ﻿open Tools.Functional
 open Domain.Game
 open Domain.Player
+open Domain.Lobby
 
-let playerX = { Designation = PlayerDesignation.X; PlayerId = (PlayerId "000000-0000-0000-000000") }
-let playerO = { Designation = PlayerDesignation.O; PlayerId = (PlayerId "000000-0000-0000-000001") }
+let planerJohn = join "John Doe"
+let planerDory = join "Dory Cutsky"
 
-let moves = [
-    { Position = { Vertical = VerticalPosition.Top; Horizontal = HorizontalPosition.Left }; Player = playerX }
-    { Position = { Vertical = VerticalPosition.Center; Horizontal = HorizontalPosition.Center }; Player = playerO }
-    { Position = { Vertical = VerticalPosition.Bottom; Horizontal = HorizontalPosition.Left }; Player = playerX }
-    { Position = { Vertical = VerticalPosition.Center; Horizontal = HorizontalPosition.Left }; Player = playerO }
-]
+let createMoves playerX playerO =
+    match playerX with
+    | Failure err -> Failure err
+    | Success gamePlayerX ->
+        match playerO with
+        | Failure err -> Failure err
+        | Success gamePlayerO -> 
+            let gamePlayerX = createGamePlayer PlayerDesignation.X gamePlayerX
+            let gamePlayerO = createGamePlayer PlayerDesignation.O gamePlayerO
+            Success [
+                { Position = { Vertical = VerticalPosition.Top; Horizontal = HorizontalPosition.Left }; Player = gamePlayerX }
+                { Position = { Vertical = VerticalPosition.Center; Horizontal = HorizontalPosition.Center }; Player = gamePlayerO }
+                { Position = { Vertical = VerticalPosition.Bottom; Horizontal = HorizontalPosition.Left }; Player = gamePlayerX }
+                { Position = { Vertical = VerticalPosition.Center; Horizontal = HorizontalPosition.Left }; Player = gamePlayerO }
+            ]
 
-let playGame: Result<Game, string> = 
+let playMoves moves = 
     moves |> Seq.fold (fun acc item -> (bind (play item)) acc)  start
+
+let createAllGameStory = 
+    let moves = createMoves planerJohn planerJohn
+    match moves with
+    | Success moves -> playMoves moves
+    | Failure err -> Failure err
+
+
 
 [<EntryPoint>]
 let main argv =
-    let playedGame = playGame
+    let playedGame = createAllGameStory
     printfn "%A" playedGame
     0
