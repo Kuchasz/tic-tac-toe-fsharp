9d2f5fef8d50a46ba3be775b638219764aa6d501
Rafal Pysz <kuchasz1@o2.pl>
2019-01-20 13:34:23 +0100

    createGuid should be an function with unit

--- Domain/Lobby.fs
+++ Domain/Lobby.fs
@@ -5,12 +5,12 @@ open Domain.Player
 open Domain.Game
 open System
 
-let createGuid =
+let createGuid () =
     Guid.NewGuid().ToString()
 
 let join planerName =
     match createPlayerName planerName with
-    | Success name -> Success { Id = PlayerId createGuid; Name = name }
+    | Success name -> Success { Id = PlayerId (createGuid ()); Name = name }
     | Failure err -> Failure err
 
 let createGamePlayer designation player =
